6718ba92483dbb1299d5310db3c3bdd4699954fc
Your Name <you@example.com>
2021-01-18 20:16:27 +0800

    picom & wp-autochange

--- autostart.sh
+++ autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 /bin/bash ~/scripts/dwm-status.sh &
-#/bin/bash ~/scripts/wp-autochange.sh &
+/bin/bash ~/scripts/wp-autochange.sh &
 #picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
-#picom -b
+picom -b
 #/bin/bash ~/scripts/tap-to-click.sh &
 #/bin/bash ~/scripts/inverse-scroll.sh &
 /bin/bash ~/scripts/setxmodmap-colemak.sh &
